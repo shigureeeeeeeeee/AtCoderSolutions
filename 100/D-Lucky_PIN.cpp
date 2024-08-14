217f42a4033d9c55ccb941bc51f7b62c9cd6e309
Tatsuto Tateishi <153858703+shigureeeeeeeeee@users.noreply.github.com>
2024-08-14 21:25:01 +0900

    546

---
+++ 100/D-Lucky_PIN.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<int> s(n);
+    for(int i = 0; i < n; i++) cin >> s[i];
+    
+    vector<int> comb;
+
+    
+}
